b70cf04e2591bdce123cdc736b912f0229b25684
Matheus Siqueira <matheus.spa140@gmail.com>
2026-02-06 00:20:08 -0300

    primeiro commit

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿
+using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public IActionResult Get()
+        {
+            var produtos = _context.Produtos;
+            return Ok(produtos);
+        }
+    }
+}
