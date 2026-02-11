83d508ac55f7769a2a41f196375eef7da846f63c
Matheus Siqueira <matheus.spa140@gmail.com>
2026-02-11 08:40:59 -0300

    CategoriasController

---
+++ ApiCatalogo/Controllers/ControllersController.cs
@@ -0,0 +1,66 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ControllersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("categorias")]
+        public ActionResult<IEnumerable<Categoria>> get()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if(categorias == null)
+            {
+                return NotFound();
+            }
+            return Ok(categorias);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Categoria> get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            return Ok(categoria);
+        }
+        [HttpPost("categorias")]
+        public ActionResult post(Categoria categoria)
+        {
+            if(categoria is null)
+            {
+                return BadRequest();
+            }
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("getCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+        [HttpDelete("categoria/{int:id}")]
+        public ActionResult delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c -> c.categoriaId == id);
+            if(categoria is null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+
+        }
+    }
+}
--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿
 using ApiCatalogo.Context;
+using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,10 +18,62 @@ namespace ApiCatalogo.Controllers
         }
 
         [HttpGet("produtos")]
-        public IActionResult Get()
+        public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos;
-            return Ok(produtos);
+            var produtos = _context.Produtos.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+        [HttpPost()]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            var produtoBanco = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produtoBanco is null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produtoBanco = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produtoBanco is null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produtoBanco);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
